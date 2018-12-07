d6b9ead977d8b4955038bbbe64c0b17820458469
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-12-07 18:23:29 +0100

    creating module and resistor components

---
+++ kresistor/resistor.py
@@ -0,0 +1,22 @@
+from kivy.uix.dropdown import DropDown
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Resistor(BoxLayout):
+    pass
+
+
+class Colour(DropDown):
+    pass
+
+class Wire(AnchorLayout):
+    pass
+
+
+class Boder(AnchorLayout):
+    pass
+
+
+class Separation(AnchorLayout):
+    pass
