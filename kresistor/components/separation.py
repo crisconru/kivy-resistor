07eae0b9231e601e800deb05867a715a0190ab54
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-12-09 17:43:18 +0100

    components declared but not implemented

---
+++ kresistor/components/separation.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from kivy.lang.builder import Builder
+from kivy.uix.anchorlayout import AnchorLayout
+
+kv_file = str(Path(__file__))[:-3] + '.kv'
+Builder.load_file(kv_file)
+
+
+class Separation(AnchorLayout):
+    pass
